54726d357ccb0fbcaedba69e15321a74ef7fdcfd
Nishanth-Shanmuganathan <theerannishanth05@gmail.com>
2021-02-15 22:19:14 +0530

    Ad bar completed
    Ad bar added along with timer running
    No pending tasks in ad bar

---
+++ js/timer.js
@@ -0,0 +1,33 @@
+var countDownDate = new Date("feb 28, 2021 23:59").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = minutes;
+    document.getElementById("second").innerHTML = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("day").innerHTML = 0;
+        document.getElementById("hour").innerHTML = 0;
+        document.getElementById("minute").innerHTML = 0;
+        document.getElementById("second").innerHTML = 0;
+    }
+}, 1000);
